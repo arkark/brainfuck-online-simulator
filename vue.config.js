5a2c0eff9402255443ff2761a33775e7bf70fe74
Takeshi Kaneko <target.ark@gmail.com>
2020-10-21 15:57:28 +0900

    Fix paths for production

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/brainfuck-online-simulator/"
+      : "/",
   configureWebpack: {
     module: {
       rules: [
